d00fbe873d78d844fc005e9c9ac518a23cf2226b
rytco <116452553+rytco@users.noreply.github.com>
2022-12-15 02:18:30 -0500

    Adding more features and fleshing out more

--- SOCProj/SOCMain.java
+++ SOCProj/SOCMain.java
@@ -1,7 +1,13 @@
 // The main file
 
+import javax.swing.*; //For the window
+import java.awt.*;
+
 public class SOCMain {
     public static void main(String args[]) {
-        
+
+        //Showing calender
+        SOCCal calenderGraphics = new SOCCal();
+        //calenderGraphics.calenderUi();
     }
 }
