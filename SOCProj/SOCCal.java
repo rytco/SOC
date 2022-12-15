d1b4f6233baac870c71fe4b50c3b8ebfb1ea9fdb
rytco <116452553+rytco@users.noreply.github.com>
2022-12-15 02:19:31 -0500

    Fleshing out more items

--- SOCProj/SOCCal.java
+++ SOCProj/SOCCal.java
@@ -1,20 +1,46 @@
 // The calander function file
 // Using JavaFx to create
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.*; //For the window
+import java.awt.*; //For colors
+import java.time.DayOfWeek;
 import java.util.*;
 
 public class SOCCal{
     //Createing the lists of days and month names
-    private static final String[] dayNames = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    private static final String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
     private static final String[] monthNames = { "January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December" };
+    
+    //Private jframe elements for storing in ram beforehand? idk
+    private JPanel userInputInternalFrame;
+    private SpringLayout userInputLayout;
+    private JButton userConfirm;
+    private JFrame calenderFrame;
+    private JTable calenderTable;
+
+    private JTextField usersName;
+    private JLabel nameLabel;
+
+
+    //Creating the JLists
+    JList monthList = new JList<>(monthNames);
+
+    //Creating class objs
+    SOCData theData = new SOCData();
 
     //Creating the calender UI
     public void calenderUi() {
-        JFrame calenderFrame = new JFrame("SOC Management APP");
+        //Creating the calender frame
+        calenderFrame = new JFrame("SOC Management APP"); 
+
+        //Adding addtional elements
+        calenderUserIn();
+
+        // Calander Size & Parameters
+        calenderFrame.setSize(1080,480);
         calenderFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        System.out.print("test");
+        calenderFrame.setResizable(false);
+        calenderFrame.setVisible(true);
     }
 
     //Creating the month panel
@@ -29,6 +55,41 @@ public class SOCCal{
 
     //Creating slot for userinput 
     public void calenderUserIn() {
+        // .setBackround(Color.green) //sets backround to green; 
+
+        
+        //Creating a internal frame to hold the userinput
+        JPanel userInputInternalFrame = new JPanel();
+        userInputInternalFrame.setLocation(10,10);
+        userInputInternalFrame.setSize(10,10);
+
+        //Creating a layout
+        SpringLayout userInputLayout = new SpringLayout();
+
+        //Adding the textfields and text entry into layout
+        JTextField usersName = new JTextField(15);
+        JLabel userNameLabel = new JLabel("Name");
+        userInputInternalFrame.add(userNameLabel);
+        userInputInternalFrame.add(usersName);
+        
+
+        //Creating the userinput confirmation button
+        JButton userConfirm = new JButton("Enter");
+        userConfirm.setBounds(100, 100, 15, 5);
+        userInputInternalFrame.add(userConfirm);
+
+        // Adding to main and showing
+        calenderFrame.add(userInputInternalFrame);
+        userInputInternalFrame.setVisible(true);
+    }
+
+    public void eventList() {
 
     }
+
+    public void actionPerformed(Action event){
+
+    }
+
+    
 }
--- SOCProj/SOCData.java
+++ SOCProj/SOCData.java
@@ -1,5 +1,43 @@
-// Local data save
+// Data Stuff
+
+import java.time.*;
+import java.util.*;
 
 class SOCData {
-    
+    public int month;
+    public int year;
+    public int currentDay;
+    public int daysOfMonth;
+
+    public long monthGet() {
+        Date date = new Date(); //Creates data obj from java.util
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(); //Gets the local date
+        int month = localDate.getMonthValue(); //uses obj above to get the month
+
+        return(month);
+    }
+
+    public long yearGet() {
+        Date date = new Date(); //Creates data obj from java.util
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(); //Gets the local date
+        int year = localDate.getYear(); //uses obj above to get the year
+
+        return(year);
+    }
+
+    public long dayGet() {
+        Date date = new Date(); //Creates data obj from java.util
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(); //Gets the local date
+        int currentDay = localDate.getDayOfMonth(); //uses obj above to get the day
+
+        return(currentDay);
+    }
+
+    public int daysInMonth() {
+        Date date = new Date(); //Creates data obj from java.util
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(); //Gets the local date
+        int daysOfMonth = localDate.lengthOfMonth(); //uses obj above to get the days in a month
+
+        return(daysOfMonth);
+    }
 }
