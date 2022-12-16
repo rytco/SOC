ac1846c3b106525472d07c9915e4568991f662c2
rytco <116452553+rytco@users.noreply.github.com>
2022-12-16 02:50:36 -0500

    theres some features i should add as noted in the todo and need to work on the button for events and such but yeah

--- soccalender/src/main/java/com/soc/SOCMain.java
+++ soccalender/src/main/java/com/soc/SOCMain.java
@@ -2,8 +2,16 @@
 
 package com.soc;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class SOCMain {
     public static void main(String args[]) {
+
+        /*
+        test  gridLayoutExample = new test();
+        gridLayoutExample.showGridLayoutDemo();
+        */
 
         //Showing calender
         SOCCal calenderGraphics = new SOCCal();
---
+++ soccalender/src/main/java/com/soc/calenderMonth.java
@@ -0,0 +1,5 @@
+package com.soc;
+
+public class calenderMonth {
+
+}
